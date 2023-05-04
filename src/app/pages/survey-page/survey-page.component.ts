af55c142e771a73fdd7bb52cdaaad606cfb6efa9
Amnish <amnishsingh04@gmail.com>
2023-05-04 15:11:03 -0400

    Survey Pagination

--- src/app/pages/survey-page/survey-page.component.ts
+++ src/app/pages/survey-page/survey-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SurveyPage } from './survey-utils/enums';
 
 @Component({
   selector: 'app-survey-page',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./survey-page.component.css']
 })
 export class SurveyPageComponent {
+    pageNumber = SurveyPage.Identification;
 
+    //#region General Getters
+    get pageTitle() {
+        return "2023 DIA SURVEY";
+    }
+
+    get surveyTitle() {
+        let title = '';
+
+        switch(this.pageNumber) {
+            case SurveyPage.Identification:
+                title = 'Personal Questions';
+                break;
+            case SurveyPage.Lifestyle:
+                title = 'Lifestyle Questions';
+                break;
+            case SurveyPage.Symptoms:
+                title = 'Symptoms Questions';
+                break;
+        }
+
+        return title;
+    }
+
+    get showSubmit() {
+        return this.pageNumber === SurveyPage.Symptoms;
+    }
+
+    //#endregion
+
+    //#region Pagination
+    nextClicked() {
+        ++this.pageNumber;
+    }
+
+    previousClicked() {
+        --this.pageNumber;
+    }
+
+    get prevDisabled() {
+        return this.pageNumber === SurveyPage.Identification;
+    }
+
+    get nextDisabled() {
+        return this.pageNumber === SurveyPage.Symptoms;
+    }
+
+    //#endregion
 }
---
+++ src/app/pages/survey-page/survey-utils/enums.ts
@@ -0,0 +1,5 @@
+export enum SurveyPage {
+    Identification  = 0,
+    Lifestyle = 1,
+    Symptoms = 2
+}
